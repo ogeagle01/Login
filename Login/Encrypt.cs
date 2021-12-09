d0ed5b6e7eb419399a3662471fd73f4faef43a6c
ogeagle01 <95836028+ogeagle01@users.noreply.github.com>
2021-12-08 21:50:56 -0600

    Add files via upload

---
+++ Login/Encrypt.cs
@@ -0,0 +1,14 @@
+//Encrypts passwords
+using System.Security.Cryptography;
+using System.Text;
+public class Encrypt
+    {
+     public static string EncryptPassword(string password) //takes a string (password) and encrypts it
+        {
+            MD5 md5 = MD5.Create(); //creates MD5 instance
+            UTF8Encoding utf8 = new UTF8Encoding(); //creates utf8 instance
+            byte[] data = md5.ComputeHash(utf8.GetBytes(password)); //Uses the md5 instance to encrypt passwords using ComputeHash, which Computes the hash value for the specified byte array. 
+            //utf8.GetBytes encodes a set of characters from the specified String into the specified byte array.
+            return Convert.ToBase64String(data); //returns the encrypted password converted into from a byte to string
+        }
+    }
---
+++ Login/Login.cs
@@ -0,0 +1,31 @@
+﻿//Base for user login
+
+Console.WriteLine("Are you a returning user? Y or N"); //Prompts user to type
+string userType = Console.ReadLine(); //User types y (yes), for returning user, or n (no), for new user
+userType = userType.ToLower(); //Lowers case to make all answers uniform
+
+if (userType == "y") //Allows a returning user to login
+{
+    if (Auth.authChk()) //Logins user with correct creds
+    {
+        Console.WriteLine("Welcome!");
+    }
+    else
+    {
+        Console.WriteLine("Wrong Login!");
+        return;
+    }
+}
+
+if (userType == "n") //Allows a new user to create an account
+{
+    if (NewUser.userCreation())
+    {
+        Console.WriteLine("ReLogin");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+    }
+}
