7bf082c19f8c2e94917ff4dc1fefc2c6f8d41fbb
ogeagle01 <95836028+ogeagle01@users.noreply.github.com>
2021-12-08 21:52:21 -0600

    Update Encrypt.cs

--- Login/Encrypt.cs
+++ Login/Encrypt.cs
@@ -9,6 +9,6 @@ public class Encrypt
             UTF8Encoding utf8 = new UTF8Encoding(); //creates utf8 instance
             byte[] data = md5.ComputeHash(utf8.GetBytes(password)); //Uses the md5 instance to encrypt passwords using ComputeHash, which Computes the hash value for the specified byte array. 
             //utf8.GetBytes encodes a set of characters from the specified String into the specified byte array.
-            return Convert.ToBase64String(data); //returns the encrypted password converted into from a byte to string
+            return Convert.ToBase64String(data); //returns the encrypted password converted from a byte to string
         }
     }
